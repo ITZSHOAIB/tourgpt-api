9ff768ae7aedd7ff6409f8a4e9c2959d25dafc12
ITZSHOAIB <skshoaibpersonal@gmail.com>
2023-05-24 16:13:31 +0530

    Swagger implemented

---
+++ src/shared/swagger/swagger.ts
@@ -0,0 +1,12 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { SWAGGER_CONFIG } from './swagger.config';
+
+export function createDocument(app: INestApplication): OpenAPIObject {
+  const builder = new DocumentBuilder()
+    .setTitle(SWAGGER_CONFIG.title)
+    .setDescription(SWAGGER_CONFIG.description)
+    .setVersion(SWAGGER_CONFIG.version);
+
+  return SwaggerModule.createDocument(app, builder.build());
+}
